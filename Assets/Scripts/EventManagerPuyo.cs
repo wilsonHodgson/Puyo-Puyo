0e42e72f61deb169405bcfa7364a7a346065a27f
Wilson Hodgson <wilsonhodgsonhodgson@cmail.carleton.ca>
2020-02-02 13:44:25 -0500

    communitive deleting between players

---
+++ Assets/Scripts/EventManagerPuyo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManagerPuyo : MonoBehaviour
+{
+    public delegate void DeleteRows(int rowNumber);
+
+    public event DeleteRows OnRowDeleteEvent;
+    
+    public void OnRowDeleteMethod(int rowNumber)
+    {
+        if (OnRowDeleteEvent != null){
+            OnRowDeleteEvent(rowNumber);
+        }
+    }
+}
